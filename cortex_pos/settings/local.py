40528330dfabb267477a525f565a7ce8649f0535
mikegyamfi <codeage20@gmail.com>
2026-03-27 16:27:03 +0000

    add gunicorn and psycopg2

--- cortex_pos/settings/local.py
+++ cortex_pos/settings/local.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'abosseyokai-hub-a4e53ad7e19b.herokuapp.com']
 
 # Database for development
 DATABASES = {
