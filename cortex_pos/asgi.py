8e5b91dfd61a71f2966f292f187aa55ce8c38642
mikegyamfi <codeage20@gmail.com>
2026-03-27 16:25:35 +0000

    add gunicorn and psycopg2

--- cortex_pos/asgi.py
+++ cortex_pos/asgi.py
@@ -1,10 +1,16 @@
+"""
+ASGI config for cortex_pos project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
 import os
+
 from django.core.asgi import get_asgi_application
 
-if os.environ.get('ENV') == 'production':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'retail_pos.settings.production')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'retail_pos.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cortex_pos.settings.local')
 
 application = get_asgi_application()
-
--- cortex_pos/wsgi.py
+++ cortex_pos/wsgi.py
@@ -1,12 +1,16 @@
+"""
+WSGI config for cortex_pos project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-# Check if the server has a variable called "ENV" set to "production"
-if os.environ.get('ENV') == 'production':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'retail_pos.settings.production')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'retail_pos.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cortex_pos.settings.local')
 
 application = get_wsgi_application()
-
-
